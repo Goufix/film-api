6ea127200bd2aea86b0c95d3a7b79fed624217e3
Goufix <alifer.17.11@gmail.com>
2022-12-07 11:44:03 -0300

    Fix: port and .env.example

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new QueryExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
